c709e912407a9a36c8ca1143f249172064f7c7e2
Gokul RANGARAJAN <gokulraja2006@mail.com>
2021-09-26 20:27:24 +0530

    Sample app to use bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Demo application</h1>
-    </div>
+    /*<div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>*/
+    <Container fluid>
+    <Row>
+      <Col xs={12}>
+        <Navbar>
+          <Container>
+            <Navbar.Brand href="#home">CI/CD Demo</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#profile">Profile</Nav.Link>
+                <Nav.Link href="#message">Messages</Nav.Link>
+                <NavDropdown title="Action" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <h3>
+          AWS CI/CD Demo - Sample
+        </h3>
+        <Row>
+          <Col xs={8}>
+            <blockquote class="blockquote">
+              <p class="mb-0">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+              </p>
+              <footer class="blockquote-footer">
+                Someone famous in <cite>Source Title</cite>
+              </footer>
+            </blockquote>
+          </Col>
+          <Col xs={4}>
+            <blockquote class="blockquote">
+              <header class="blockquote-footer">
+                Message board
+              </header>
+              <p class="mb-0">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+              </p>
+              <footer class="blockquote-footer">
+                Someone famous in <cite>Source Title</cite>
+              </footer>
+            </blockquote>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  </Container>
   );
 }
 
