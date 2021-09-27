8c02566116d20057b41b65ba57949dcb2166f1fe
Gokul RANGARAJAN <gokulraja2006@mail.com>
2021-09-27 14:25:12 +0530

    Fixed test failures

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/AWS CI\/CD Demo - Sample/i);
   expect(linkElement).toBeInTheDocument();
 });
