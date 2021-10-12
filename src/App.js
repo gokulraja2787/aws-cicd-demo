e150afe943d815de0b930c6d8d87bdc43443b3fa
Gokul RANGARAJAN <gokulraja2006@mail.com>
2021-10-12 23:45:40 +0530

    Demo commit to test CD

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
                 <NavDropdown title="Action" id="basic-nav-dropdown">
                   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Someother action</NavDropdown.Item>
                   <NavDropdown.Divider />
                   <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                 </NavDropdown>
